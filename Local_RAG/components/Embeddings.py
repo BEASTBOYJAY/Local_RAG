38d97c5b540bd1502b4e7107ed187e9e00f3ab35
BEASTBOYJAY <jay31sinha@gmail.com>
2024-12-24 16:58:06 +0530

    Added Embeddings.py to generate and save embeddings from the processed PDF content.

---
+++ Local_RAG/components/Embeddings.py
@@ -0,0 +1,36 @@
+from sentence_transformers import SentenceTransformer
+import torch
+from tqdm import tqdm
+from PDF_Processing import PDF_Processor
+import pandas as pd
+
+
+class Save_Embeddings:
+    def __init__(self, embedding_model="all-mpnet-base-v2"):
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.pdf_processor = PDF_Processor("human-nutrition-text.pdf")
+        self.pages_and_chunks = self.pdf_processor.run()
+
+        self.embedding_model = SentenceTransformer(
+            model_name_or_path=embedding_model, device=self.device
+        )
+
+    def _generate_embeddings(self):
+        for item in tqdm(self.pages_and_chunks, desc="generating embeddings"):
+            item["embedding"] = self.embedding_model.encode(
+                item["sentence_chunk"], convert_to_tensor=True
+            )
+
+    def _save_embeddings(self):
+        data_frame = pd.DataFrame(self.pages_and_chunks)
+        data_frame.to_csv("embeddings.csv", index=False)
+
+    def run(self):
+        self._generate_embeddings()
+        self._save_embeddings()
+        return self.pages_and_chunks
+
+
+if __name__ == "__main__":
+    save_embeddings = Save_Embeddings()
+    save_embeddings.run()
